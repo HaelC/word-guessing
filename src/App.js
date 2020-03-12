179fdd41372d146ee9402fd706971f1cfcb83adb
Hael Chan <f.procumbens@gmail.com>
2020-03-12 13:38:35 -0700

    remount Play component when click play again

--- src/App.js
+++ src/App.js
@@ -4,38 +4,17 @@ import Header from "./components/layout/Header";
 import Play from "./components/pages/Play";
 import About from "./components/pages/About";
 import "./App.css";
-// import Level from "./components/Level";
 
 function App() {
-  // const [level, setLevel] = useState(-1);
-
-  // const changeLevel = selectedLevel => {
-  //   setLevel(selectedLevel);
-  // };
-
-  // if (level === -1) {
-  //   return (
-  //     <div className="App">
-  //       <Level changeLevel={changeLevel} />
-  //     </div>
-  //   );
-  // }
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       {/* <Level changeLevel={changeLevel} /> */}
-  //       <p>{level}</p>
-  //     </header>
-  //   </div>
-  // );
-
   return (
     <Router>
       <div className="App">
         <div className="container">
           <Header />
           <Route exact path="/" />
-          <Route path="/play" component={Play} />
+          {/* <Route path="/play" component={Play} /> */}
+          {/* https://stackoverflow.com/a/57787353 */}
+          <Route path="/play" render={() => <Play key={Date.now()} />} />
           <Route path="/about" component={About} />
         </div>
       </div>
